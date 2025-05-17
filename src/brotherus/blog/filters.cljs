cd8db8117ac9a119955257b2482bc9eb6310e1e6
Robert Brotherus <robert@iki.fi>
2025-05-17 18:24:51 +0300

    Sort articles with latest first

--- src/brotherus/blog/filters.cljs
+++ src/brotherus/blog/filters.cljs
@@ -7,7 +7,8 @@
 (defn filter-articles [articles tag]
   (if-not tag articles
               (->> articles
-                   (filter (fn [{tags :tags}] (get tags tag))))))
+                   (filter (fn [{tags :tags}] (get tags tag)))
+                   (sort-by :date #(compare %2 %1)))))
 
 ;; Subs
 
