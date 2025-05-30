b77aea96a42fc407677c329a65fd7eb85b6600a3
Robert Brotherus <robert@iki.fi>
2025-05-31 00:39:33 +0300

    deployment script fixed

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,7 @@
 npm install
 Remove-Item -Recurse -Force .\resources\public\js\compiled
 npm run release
-aws s3 cp .\resources\public  s3://woolcat.fi/ --recursive --cache-control "no-cache, no-store, must-revalidate"
-# Deploys to: http://woolcat.fi.s3-website.eu-north-1.amazonaws.com/
-# Mapped to Cloudfront disribution: https://d2l603lgj6s6a9.cloudfront.net/
-# Mapped in Route 53 to https://woolcat.fi
+aws s3 cp .\resources\public  s3://brotherus.net/ --recursive --cache-control "no-cache, no-store, must-revalidate"
+# Deploys to: http://brotherus.net.s3-website.eu-north-1.amazonaws.com  -> OK!
+# Mapped to Cloudfront disribution: https://d34ov319dta0zj.cloudfront.net  -> OK!
+# Mapped in Route 53 to https://brotherus.net or https://www.brotherus.net ->
