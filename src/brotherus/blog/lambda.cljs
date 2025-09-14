a87e214ad3560db95f3c4c46429c7c34dc59b37f
Robert Brotherus <robert@iki.fi>
2025-09-15 00:30:07 +0300

    Refactor handle-post

--- src/brotherus/blog/lambda.cljs
+++ src/brotherus/blog/lambda.cljs
@@ -40,6 +40,25 @@
                (let [hiccup-content (article/markdown-to-hiccup markdown {:item-id "about"})]
                  (render/render-about-page hiccup-content))))))
 
+(defn serve-article [article-info id articles]
+  (let [url (or (:url article-info) (str id "/article.md"))]
+    (-> (increment-view-counter id)
+        (.then (fn [new-count]
+                 (-> (fetch-article-content url)
+                     (.then (fn [markdown]
+                              (let [hiccup-content (article/markdown-to-hiccup markdown {:item-id id})]
+                                (render/render-article-page (assoc article-info :views new-count)
+                                                            hiccup-content articles))))))))))
+
+(defn redirect-to-short-id [short-id]
+  (js/Promise.resolve
+    {:statusCode 301
+     :headers {"Location" (str "/post/" short-id)
+               "Content-Type" "text/html; charset=utf-8"}
+     :body (str "<!DOCTYPE html><html><head><title>Redirecting...</title></head>"
+                "<body><p>Redirecting to <a href=\"/post/" short-id "\">/post/" short-id "</a></p>"
+                "<script>window.location.href='/post/" short-id "';</script></body></html>")}))
+
 (defn handle-post [id]
   ;; First fetch articles from GitHub API, then handle the request
   (-> (github/get-cached-articles)
@@ -48,29 +67,13 @@
                      articles-index2 (github/create-articles-index2 articles)]
                  (if-let [article-info (get articles-index id)]
                    ;; Found by short ID - serve the article
-                   (let [url (or (:url article-info) (str id "/article.md"))]
-                     (-> (increment-view-counter id)
-                         (.then (fn [new-count]
-                                  (-> (fetch-article-content url)
-                                      (.then (fn [markdown]
-                                               (let [hiccup-content (article/markdown-to-hiccup
-                                                                      markdown {:item-id id})]
-                                                 (render/render-article-page
-                                                   (assoc article-info :views new-count)
-                                                   hiccup-content articles)))))))))
-                 ;; Not found by short ID - check if it's a long ID
-                 (if-let [article-info (get articles-index2 id)]
-                   ;; Found by long ID - redirect to short ID
-                   (let [short-id (:id article-info)]
-                     (js/Promise.resolve
-                       {:statusCode 301
-                        :headers {"Location" (str "/post/" short-id)
-                                  "Content-Type" "text/html; charset=utf-8"}
-                        :body (str "<!DOCTYPE html><html><head><title>Redirecting...</title></head>"
-                                   "<body><p>Redirecting to <a href=\"/post/" short-id "\">/post/" short-id "</a></p>"
-                                   "<script>window.location.href='/post/" short-id "';</script></body></html>")}))
-                   ;; Not found in either index - 404
-                   (js/Promise.resolve (render/render-404-page)))))))) )
+                   (serve-article article-info id articles)
+                   ;; Not found by short ID - check if it's a long ID
+                   (if-let [article-info (get articles-index2 id)]
+                     ;; Found by long ID - redirect to short ID
+                     (redirect-to-short-id (:id article-info))
+                     ;; Not found in either index - 404
+                     (js/Promise.resolve (render/render-404-page)))))))))
 
 (defn handle-posts [tag]
   (-> (github/get-cached-articles)
